a747d5d27066ceeee60c009e49db7849991e44a9
Dis-g <disanti04ghosh@gmail.com>
2026-01-04 00:33:05 +0530

    edited typos and gradient

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,8 +7,8 @@ import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Resumind" },
-    { name: "description", content: "Smart feedback for your dream job!" },
+    { title: "CVify" },
+    { name: "description", content: "Smart AI feedback for your dream job!" },
   ];
 }
 
